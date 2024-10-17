54adcf0321dcbb3c1fb7d112243d9ec7e54940b1
Jordan Sumner <144553157+JordanSum@users.noreply.github.com>
2024-10-17 14:41:40 -0700

    enhace security practice

--- modules/EC2/SG.tf
+++ modules/EC2/SG.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "terraform-SG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<your-IP-address/32>"]
   }
 
   egress {
